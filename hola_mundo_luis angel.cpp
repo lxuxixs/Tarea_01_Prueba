a9d2354eba22555ae2b4a29b066284cacf261c8a
lxuxixs <79498654+lxuxixs@users.noreply.github.com>
2021-02-25 21:31:00 -0600

    Add files via upload

---
+++ hola_mundo_luis angel.cpp
@@ -0,0 +1,12 @@
+   #include <iostream>
+
+using namespace std;
+using std :: cin;
+using std :: string;
+
+int main(){
+    string name;
+    cout <<"introdcuce tu nombre";
+    cin >> name;
+    cout << "hola" << name ;
+    }
